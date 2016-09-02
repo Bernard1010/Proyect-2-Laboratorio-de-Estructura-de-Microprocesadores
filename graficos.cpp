ce3ae181ec97b4810629db432cf6c13a2ce1e7ec
fepi12 <pipeherrero@gmail.com>
2016-09-02 14:18:01 -0600

    Update graficos.cpp
    En esta actualizacion se incluye:
    -Ciclo de vidas: El jugador tiene 3 intentos para ganar el nivel antes de perder
    -Animaciones de muerte y contador de vidas: El simbolo de las vidas tiene animacion y al momento de perder se genera una explosion
    -Pausa entre vidas: El jugador podra mover la barra antes de que inicie el nivel. Para iniciar se debe presionar la tecla x
    -Modo de rastreo: Para facilitar las pruebas se incluye un modo de rastreo en donde la barra siempre estara debajo de la bola sin posibilidad de perder
    -Arreglo de colisiones con los nuevos tamanos de pantalla para aumentar la velocidad de juego

--- graficos.cpp
+++ graficos.cpp
@@ -23,6 +23,11 @@ BITMAP *borde;
 
 BITMAP *trueno;								//variable para guardar imagen de animacion del trueno
 
+BITMAP *boomBMP;
+BITMAP *boom;
+BITMAP *svida;
+BITMAP *svidaBMP;
+
 
 int sizescreen_x=548;						//tamano horizontal de la ventana de juego
 int sizescreen_y=650;						//Tamano vertical de la ventana de juego
@@ -30,10 +35,10 @@ int sizescreen_y=650;						//Tamano vertical de la ventana de juego
 int mh=0;									//variable para control de movimiento horizontal de la bola
 int mv=1;									//variable de control de movimiento vertical de la bola
 int bpx=239;								//Variable de posicion de en x de la bola
-int bpy=555+12+7;								//Variable de posicion de en y de la bola
+int bpy=555-12-7;								//Variable de posicion de en y de la bola
 
 int vbola=2;								//velocidad de la bola [pixeles/cilo]
-int vbarra=3;								//velocidad de la barra [pixeles/cilo]
+int vbarra=4;								//velocidad de la barra [pixeles/cilo]
 
 
 int dir=0;						
@@ -47,7 +52,19 @@ int postry=570;								//Posicion vertical trueno
 int holdtr=0;								//Cuenta de animacion del trueno`
 int veltr=40;								//Variable de velocidad de animacion del trueno
 
+int vidas=3;									//Variable de numero de vidas
+
+int Avidas=0;								//Variable de animacion de las vidas
+int holdvidas=0;							//Cuenta de animacion de vidas
+int velvidas=10;							//Variable de velocidad de animacion de vidas
+
+int muerte=0;								//Variable de animacion del trueno
+int holdmuerte=0;							//Cuenta de animacion de muerte
+int velmuerte=10;							//Variable de velocidad de animacion de muerte
+int ciclomuerte=0;							//Variable de control de juego (indica que perdio una vida)
+
 char matrizbloques [21][14];
+
 char bloquesombrilla [21][14]={
 	"      D      ",
 	"     EEE     ",
@@ -232,7 +249,7 @@ void movimientobola()
 		if(mh==1)//Si mh es igual a 1, la bola se mueve hacia la derecha
 		{
 			if(bpx+2>=sizescreen_x-33) mh=0;	
-			else if((bpx+14 >=px && bpx+9 <= px) && (bpy+14>=py && bpy <= py+32))
+			else if((bpx+8 >=px && bpx+6 <= px) && (bpy+8>=py && bpy <= py+32))
 			{
 				mh=0;
 			}			
@@ -259,7 +276,9 @@ void movimientobola()
 		{
 			if(bpy>=postry-7)
 			{
-				mv=1;
+				mv=2;
+				ciclomuerte=1;
+				mh=2;
 			}
 			else if((bpy+10 >=py && bpy+13 <= py+5) && (bpx+14>=px && bpx <= px+64))
 			{
@@ -348,6 +367,113 @@ void animtrueno()//Funcion de animacion del trueno (piso)
 }	
 
 
+void animmuerte()
+{
+	if (holdmuerte == velmuerte && muerte==0)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 1;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==1)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 2;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==2)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 3;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==3)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 4;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==4)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 5;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==5)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 6;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==6)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 7;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==7)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 8;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==8)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 9;
+			holdmuerte = 0;
+		}			
+		
+	else if (holdmuerte == velmuerte && muerte==9)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 10;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==10)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 11;
+			holdmuerte = 0;
+		}
+		
+	else if (holdmuerte == velmuerte && muerte==11)
+		{
+			blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);
+			draw_sprite(buffer,boom,bpx,bpy);
+			muerte = 0;
+			holdmuerte = 0;
+		}
+		
+	else 
+		{
+			holdmuerte++;
+			draw_sprite(buffer,boom,bpx,bpy);
+		}
+		
+}
+
+
 void comparabloques(char matriz[21][14])
 {
 	
@@ -358,7 +484,7 @@ void comparabloques(char matriz[21][14])
 					
 					if(mv==0)
 					{
-						if(bpy+14>=23+16*i && bpy<=23+2+16*i && bpx>=18+39*j && bpx+14<+22+39*j+39)
+						if(bpy+8>=23+16*i && bpy+8<=23+16*i+2 && bpx>=18+39*j-1 && bpx+8<=22+39*j+39+1)
 						{
 							if(matriz[i][j]!=' ')
 							{
@@ -370,7 +496,7 @@ void comparabloques(char matriz[21][14])
 				    }		
 					if(mv==1)
 					{
-						if(bpy<=23+16+16*i && bpy>=23+14+16*i && bpx+14>=18+39*j && bpx<=22+39*j+39)
+						if(bpy<=23+16+16*i && bpy>=23+4+16*i && bpx>=18+39*j && bpx+8<=22+39*j+39)
 						{
 							if(matriz[i][j]!=' ')
 							{
@@ -384,7 +510,7 @@ void comparabloques(char matriz[21][14])
 			//////////////////////////////////////////////////
 					if(mh==0)
 					{
-						if(bpy+14>=23+16*i && bpy<=23+16*i+16 && bpx<=18+39*j+39 && bpx>=18+39*j-1)
+						if(bpy+8>=23+16*i && bpy<=23+16*i+16 && bpx<=18+39*j+39 && bpx>=18+39*j)
 						{
 							if(matriz[i][j]!=' ')
 							{
@@ -396,7 +522,7 @@ void comparabloques(char matriz[21][14])
 				    }		
 					 if(mh==1)
 					{
-						if(bpy+14>=23+16*i && bpy<=23+16*i+16 && bpx+14>=18+39*j && bpx+14<=18+39*j+1)
+						if(bpy+8>=23+16*i && bpy<=23+16*i+16 && bpx+8>=18+39*j && bpx+8<=18+39*j+1)
 						{
 							if(matriz[i][j]!=' ')
 							{
@@ -433,53 +559,225 @@ void cargar(char matriz_in[21][14],char matriz_out[21][14])
 }
 
 
+void animvidas()
+{
+	if (holdvidas == velvidas && Avidas==0)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 1;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==1)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 2;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==2)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 3;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==3)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 4;
+			holdvidas = 0;
+		}
+	
+	else if (holdvidas == velvidas && Avidas==4)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 5;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==5)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 6;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==6)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 7;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==7)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 8;
+			holdvidas = 0;
+		}		
+		
+	else if (holdvidas == velvidas && Avidas==8)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 9;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==9)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 10;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==10)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 11;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas && Avidas==11)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 12;
+			holdvidas = 0;
+		}
+		
+	else if (holdvidas == velvidas*4 && Avidas==12)
+		{
+			blit(svidaBMP,svida,(-2+62)*Avidas,0,0,0,62,25);
+			draw_sprite(buffer,svida,106,610);
+			Avidas = 0;
+			holdvidas = 0;
+		}
+		
+	else holdvidas++;						
+}
 
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() 
 {
-		init_allegro();
+		init_allegro();										//Funcion que inicia las funciones de allegro
 		     
-        buffer=create_bitmap(sizescreen_x,sizescreen_y);
-        fondo = load_bitmap("fondo.bmp",NULL);
-        borde = load_bitmap("borde.bmp",NULL);
-        barra = load_bitmap("barra.bmp",NULL);
-        bola = load_bitmap("BolaR.bmp",NULL);
-        bloque0 = load_bitmap("bloque0.bmp",NULL);
-        bloque1 = load_bitmap("bloque1.bmp",NULL);
-        bloque2 = load_bitmap("bloque2.bmp",NULL);
-        bloque3 = load_bitmap("bloque4.bmp",NULL);
-        bloque4 = load_bitmap("bloque3.bmp",NULL);
-        trueno = load_bitmap("trueno1.bmp",NULL);
+        buffer=create_bitmap(sizescreen_x,sizescreen_y);	//Creacion de espacio de juego
+        fondo = load_bitmap("fondo.bmp",NULL);				//^
+        borde = load_bitmap("borde.bmp",NULL);				//^
+        barra = load_bitmap("barra.bmp",NULL);				//^
+        bola = load_bitmap("BolaR.bmp",NULL);				//^
+        bloque0 = load_bitmap("bloque0.bmp",NULL);			//^
+        bloque1 = load_bitmap("bloque1.bmp",NULL);			//^
+        bloque2 = load_bitmap("bloque2.bmp",NULL);			//^
+        bloque3 = load_bitmap("bloque4.bmp",NULL);			//^
+        bloque4 = load_bitmap("bloque3.bmp",NULL);			//^
+        trueno = load_bitmap("trueno1.bmp",NULL);			//^
+        boomBMP = load_bitmap("explosion.bmp",NULL);		//^
+        boom = create_bitmap(14,14);						//^
+        svidaBMP = load_bitmap("vida.bmp",NULL);			//^
+        svida = create_bitmap(62,25);						//Carga de imagenes en los bitmaps declarados
         
-       
-		//initmatriz();
-		cargar(bloquesdefecto,matrizbloques);
+        blit(svidaBMP,svida,(-2+62)*12,0,0,0,62,25);		//Corte de BMP para imprimir seccion especifica
+		draw_sprite(buffer,svida,106,610);					//Carga de corte del BMP
+		blit(boomBMP,boom,0+14*muerte,0,0,0,14,14);			//Corte de BMP para imprimir seccion especifica
+		draw_sprite(buffer,boom,bpx,bpy);					//Carga de corte del BMP
+		
         
-        while(!key[KEY_ESC])
+        
+		cargar(bloquesdefecto,matrizbloques);				//Funcion para cargar matriz de juego temporal desde
+															//otra matriz de chars declarada
+		
+     
+     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     
+     while (vidas != 0)						//Juego (Contiene el juego principal y el ciclo de vidas)
+     {
+		 vidas--;							//Reduccion de vida
+		 px=225;							//Posicion inicial barra
+		 bpx=239;							//Regresa la bola a su posicion inicial 								
+		 bpy=555-12-7;						//""
+		 ciclomuerte=0;						//Reinicia condicion de variables del ciclo de juego
+		 muerte = 0;						//""
+		 mv = 0;							//""
+		 mh = 0;							//""
+		 
+		 while (!key[KEY_X])	//////////////////////////////////////////////////////////////////////Pausa entre vidas
+		 {
+			 draw_sprite(buffer,fondo,0,0); 	//Se dibuja la imagen de fondo en el buffer
+			 animtrueno();						//Funcion de animacion del trueno
+			 draw_sprite(buffer,borde,0,0);		//Dibuja el borde en el buffer
+			 movbarra();						//Funcion de movimiento y actualizacin de barra
+			 draw_sprite(buffer,bola,px+27-5,555-10);	//Imprime en buffer el sprite de la bola sobre la barra
+			 bpx=px+27-5;						//Actualiza la posicion de la bola sobre la barra
+			 draw_sprite(buffer,svida,106,610);	//Imprime en buffer la animacion de las vidas
+			 imprimirbloques(matrizbloques);	//Imprime en buffer la matriz de bloques actualizada
+			 animvidas();						//Funcion de animacion de contador de vidas
+			 draw_sprite(buffer,svida,106,610);	//Imprime en buffer la animacion de las vidas
+			 pantalla();						//Actualiza la pantalla con el contenido del buffer
+			 clear(buffer);						//Se limpia los contenidos del buffer
+			 usleep(7000);						//Ciclo de retraso (se debe cambiar el valor dependiendo del hardware)
+			 
+		 }
+		 
+        while(!key[KEY_ESC] && ciclomuerte==0) ////////////////////////////////////////////////////////Ciclo de juego
         {
              
               
-             
-             draw_sprite(buffer,fondo,-10,0); 
-             comparabloques(matrizbloques);
-             imprimirbloques(matrizbloques);
-             
-             
-             animtrueno();
-             draw_sprite(buffer,borde,0,0); 
-             movbarra();
-             movimientobola();
+             if (key[KEY_G])					//Activacion de modo rastreo (para propositos de pruebas de bugs)
+             {
+				 px=bpx-27+5;					//La posicion de la barra siempre estara por debajo de la bola
+			 }
+			 
+			 
+             draw_sprite(buffer,fondo,0,0); 	//Se dibuja la imagen de fondo en el buffer
+             comparabloques(matrizbloques);		//Funcion de verificacion de choques
              
              
-             pantalla();
-             usleep(4000);
-             clear(buffer);
+             animtrueno();						//Funcion de animacion del trueno
+             draw_sprite(buffer,borde,0,0);		//Dibuja el borde en el buffer	 
+             movbarra();						//Funcion de movimiento y actualizacin de barra
+             movimientobola();					//Funcion de movimiento y actualizacin de bola
+             imprimirbloques(matrizbloques);	//Imprime en buffer la matriz de bloques actualizada
+             animvidas();						//Funcion de animacion de contador de vidas
+             draw_sprite(buffer,svida,106,610);	//Imprime en buffer la animacion de las vidas
+             
+             pantalla();						//Actualiza la pantalla con el contenido del buffer
+             usleep(7000);						//Ciclo de retraso (se debe cambiar el valor dependiendo del hardware)
+             clear(buffer);						//Se limpia los contenidos del buffer
                                        
              
         }
-
+        
+        
+        while (muerte != 11) /////////////////////////////////////////////////////////////////////////Ciclo de muerte
+        {
+			draw_sprite(buffer,fondo,0,0); 		//Se dibuja la imagen de fondo en el buffer
+			animtrueno();						//Funcion de animacion del trueno
+			draw_sprite(buffer,borde,0,0);		//Dibuja el borde en el buffer	
+			draw_sprite(buffer,svida,106,610); 	//Imprime en buffer la animacion de las vidas
+			imprimirbloques(matrizbloques);		//Imprime en buffer la matriz de bloques actualizada
+			movbarra();							//Funcion de movimiento y actualizacin de barra
+			animmuerte();						//Funcion de la animacion de muerte
+			
+			pantalla();							//Actualiza la pantalla con el contenido del buffer
+			usleep(5000);						//Ciclo de retraso (se debe cambiar el valor dependiendo del hardware)
+			clear(buffer);						//Se limpia los contenidos del buffer
+		}
+	}	
 }
 END_OF_MAIN()
 
